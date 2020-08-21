671919a24a1628ac84fb703e82272b681b746da6
Rocio Dominguez Vidana <rdv.chio@gmail.com>
2020-08-21 13:40:38 -0500

    first commit with gdc functions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setuptools
+
+setup(name='mutation-gatherer',
+      version='0.1',
+      description='Gather information from NCI GDC API, ClinVar API, and specific CSV/TSV',
+      url='https://github.com/samaco-lab/mutation-gatherer',
+      author='Rocio Dominguez Vidana, PhD',
+      author_email='rocio.vidana@alumni.bcm.edu',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
