ec75dcc994de36741b90ef7bd1066567feaa1a46
Rocio Dominguez Vidana <rdv.chio@gmail.com>
2020-08-25 21:27:20 -0500

    setup new files and include parashardhapola lollipop initial code

---
+++ mutation-gatherer/interact_with_ensembl.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def query_ensembl_lookup:
+    '''
+    Interact with Ensembl API ().
+
+    Parameters:
+
+    Returns:
+
+    '''	
+	pass
+
+def main():
+    '''
+    Run as:
+
+    import sys
+    sys.path.append('/path/to/mutation-gatherer/mutation-gatherer/')
+    import interact_with_ensembl as ensembl
+
+    '''
+    pass    
+
+
+if __name__ == "__main__":
+    main()
