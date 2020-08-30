e7c1cfb01cd8383f6833397b16af493750172988
Rocio Dominguez Vidana <rdv.chio@gmail.com>
2020-08-30 00:52:42 -0500

    add package files

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setuptools
+from setuptools import setup
+
+with open('README.rst') as f:
+	readme = f.read()
+
+with open('LICENSE') as f:
+	license = f.read()
 
 setup(name='mutation-gatherer',
-      version='0.1',
-      description='Gather information from NCI GDC API, ClinVar API, and specific CSV/TSV',
-      url='https://github.com/samaco-lab/mutation-gatherer',
-      author='Rocio Dominguez Vidana, PhD',
-      author_email='rocio.vidana@alumni.bcm.edu',
-      license='MIT',
-      packages=['funniest'],
-      zip_safe=False)
+	version='0.2',
+	description='Gather variant information from NCI GDC API, ClinVar API, and specific CSV/TSV and make a variant lollipop plot',
+	url='https://github.com/samaco-lab/mutation-gatherer',
+	long_description=readme,
+	author='Rocio Dominguez Vidana, PhD',
+	author_email='rocio.vidana@alumni.bcm.edu',
+	license='MIT',
+	packages=['mutation-gatherer']
+)
